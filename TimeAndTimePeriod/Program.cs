2cd02400d2708e2be7ee5710cc9d63b5f3f2bca6
Zofia Oknińska <zofia.okninska@microsoft.wsei.edu.pl>
2023-05-23 15:46:15 +0200

    Create basic file structure

---
+++ TimeAndTimePeriod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimeAndTimePeriod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+        }
+    }
+
+}
+
+
+
+
